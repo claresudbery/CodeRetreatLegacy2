8786596c54f85f06f3fbc5fd91e74dc6c5f516b0
clare sudbery <clare.sudbery@laterooms.com>
2015-06-17 10:23:58 +0100

    Initial Code as provided by Mike

---
+++ Ferry.Tests/GoldenMasterTests.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using FerryLegacy;
+using NUnit.Framework;
+
+namespace Ferry.Tests
+{
+    [TestFixture]
+    public class GoldenMasterTests
+    {
+        //[Test]
+        public void generate_golden_master()
+        {
+            WriteToFile("master.txt");
+        }
+
+        private static void WriteToFile(string fileName)
+        {
+            TextWriter file = new StreamWriter(fileName);
+            System.Console.SetOut(file);
+            Program.MainWithTestData();
+            file.Close();
+        }
+
+        [Test]
+        public void compare_to_golden_master()
+        {
+            WriteToFile("test-run.txt");
+            var master = new StreamReader("master.txt").ReadToEnd();
+            var tests = new StreamReader("test-run.txt").ReadToEnd();
+            Assert.That(tests, Is.EqualTo(master));
+        }
+    }
+}
